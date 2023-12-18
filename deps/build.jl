55afc049ab03a825ab286327123259974755f297
Aaruni Kaushik <akaushik@mathematik.uni-kl.de>
2023-12-18 16:57:06 +0100

    Add build.jl

---
+++ deps/build.jl
@@ -0,0 +1,5 @@
+# this file is executed when the package is first installed to the system
+# what exactly does "first install" mean?
+
+using Pkg
+Pkg.Registry.add(RegistrySpec(url = "git@github.com:aaruni96/blessed-by-oscar.git"))
